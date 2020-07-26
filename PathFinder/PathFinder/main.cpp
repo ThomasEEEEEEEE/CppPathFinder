474a52779533472c7775b795a04c038aac3c1a57
Thomas Eberhart <thomas.eberhart@oit.edu>
2020-07-26 15:27:57 -0700

    Added a basic map

--- PathFinder/PathFinder/main.cpp
+++ PathFinder/PathFinder/main.cpp
@@ -1,27 +1,91 @@
 #define OLC_PGE_APPLICATION
+#include <vector>
+using std::vector;
 #include "olcPixelGameEngine.h"
 
-class Example : public olc::PixelGameEngine
+enum TileState
+{
+	EMPTY = 0,
+	START,
+	BLOCKED,
+	END
+};
+
+class Tile
 {
 public:
-	Example()
+	Tile(TileState s)
 	{
-		sAppName = "Example";
+		state = s;
+	}
+	TileState state;
+};
+
+class Finder : public olc::PixelGameEngine
+{
+public:
+	Finder()
+	{
+		sAppName = "Path Finder";
 	}
 
 public:
+	int AppWidth = ScreenWidth();
+	int AppHeight = ScreenHeight();
+	int MapWidth = 64;
+	int MapHeight = 36;
+	int TileSize = 16;
+	Tile * StartTile = nullptr;
+	Tile * EndTile = nullptr;
+	vector<Tile> Map;
+
 	bool OnUserCreate() override
 	{
-		// Called once at the start, so create things here
+		for (int i = 0; i < MapWidth * MapHeight; ++i)
+		{
+			Map.push_back(Tile(EMPTY));
+		}
 		return true;
 	}
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
-		// called once per frame
-		for (int x = 0; x < ScreenWidth(); x++)
-			for (int y = 0; y < ScreenHeight(); y++)
-				Draw(x, y, olc::Pixel(rand() % 256, rand() % 256, rand() % 256));
+		Clear(olc::BLACK);
+		
+		if (GetKey(olc::CTRL).bHeld)
+		{
+			int index = (GetMouseY() / TileSize) * MapWidth + (GetMouseX() / TileSize);
+			if (GetMouse(0).bPressed)
+			{
+				Map[index].state = START;
+			}
+			else if (GetMouse(1).bPressed)
+			{
+				Map[index].state = END;
+			}
+		}
+		else if (GetMouse(0).bPressed)
+		{
+			int index = (GetMouseY() / TileSize) * MapWidth + (GetMouseX() / TileSize);
+			if (Map[index].state % 2 == 0)
+				Map[index].state = static_cast<TileState>((Map[index].state + 2) % 4);
+		}
+
+		for (int i = 0; i < MapWidth; ++i)
+		{
+			for (int j = 0; j < MapHeight; ++j)
+			{
+				if (Map[j * MapWidth + i].state == EMPTY)
+					FillRect(i * TileSize + 2, j * TileSize + 2, TileSize - 4, TileSize - 4, olc::BLUE);
+				else if (Map[j * MapWidth + i].state == BLOCKED)
+					FillRect(i * TileSize + 2, j * TileSize + 2, TileSize - 4, TileSize - 4, olc::GREY);
+				else if (Map[j * MapWidth + i].state == START)
+					FillRect(i * TileSize + 2, j * TileSize + 2, TileSize - 4, TileSize - 4, olc::GREEN);
+				else if (Map[j * MapWidth + i].state == END)
+					FillRect(i * TileSize + 2, j * TileSize + 2, TileSize - 4, TileSize - 4, olc::RED);
+			}
+		}
+
 		return true;
 	}
 };
@@ -29,8 +93,8 @@ public:
 
 int main()
 {
-	Example demo;
-	if (demo.Construct(256, 240, 4, 4))
+	Finder demo;
+	if (demo.Construct(1024, 576, 1, 1))
 		demo.Start();
 
 	return 0;
