0da3ce303a3b246645b455f064337adafb7e59da
Thomas Eberhart <thomas.eberhart@oit.edu>
2020-07-28 17:36:19 -0700

    Began work on implementation of A* algorithm

--- PathFinder/PathFinder/main.cpp
+++ PathFinder/PathFinder/main.cpp
@@ -14,11 +14,23 @@ enum TileState
 class Tile
 {
 public:
-	Tile(TileState s)
+	Tile(int X, int Y, TileState s)
 	{
 		state = s;
+		x = X;
+		y = Y;
+		g = 1;
+		h = 0;
+		f = INT_MAX;
+		parent = nullptr;
 	}
 	TileState state;
+	int x;
+	int y;
+	int g;
+	int h;
+	int f;
+	Tile* parent;
 };
 
 class Finder : public olc::PixelGameEngine
@@ -43,7 +55,7 @@ public:
 	{
 		for (int i = 0; i < MapWidth * MapHeight; ++i)
 		{
-			Map.push_back(Tile(EMPTY));
+			Map.push_back(Tile(i % MapWidth, i % MapHeight, EMPTY));
 		}
 		return true;
 	}
@@ -57,17 +69,27 @@ public:
 			int index = (GetMouseY() / TileSize) * MapWidth + (GetMouseX() / TileSize);
 			if (GetMouse(0).bPressed)
 			{
+				if (StartTile != nullptr) StartTile->state = EMPTY;
 				Map[index].state = START;
+				StartTile = &Map[index];
+
+				if (StartTile && EndTile)
+					DoAStar();
 			}
 			else if (GetMouse(1).bPressed)
 			{
+				if (EndTile != nullptr) EndTile->state = EMPTY;
 				Map[index].state = END;
+				EndTile = &Map[index];
+
+				if (StartTile && EndTile)
+					DoAStar();
 			}
 		}
 		else if (GetMouse(0).bPressed)
 		{
 			int index = (GetMouseY() / TileSize) * MapWidth + (GetMouseX() / TileSize);
-			if (Map[index].state % 2 == 0)
+			if (Map[index].state % 2 == 0) //Only change state if not start or end
 				Map[index].state = static_cast<TileState>((Map[index].state + 2) % 4);
 		}
 
@@ -88,8 +110,86 @@ public:
 
 		return true;
 	}
-};
 
+	void ClearMap()
+	{
+		for (int i = 0; i < MapWidth * MapHeight; ++i)
+		{
+			Map[i].g = 1;
+			Map[i].f = 0;
+			Map[i].h = 0;
+			Map[i].parent = nullptr;
+		}
+	}
+
+	vector<Tile> DoAStar()
+	{
+		auto GetDistance = [](Tile* a, Tile* b)
+		{
+			return sqrtf((a->x - b->x) * (a->x - b->x) + (a->y - b->y) * (a->y - b->y));
+		};
+
+		ClearMap();
+
+		Tile* Current = StartTile;
+		vector<Tile*> OpenList;
+		vector<Tile*> ClosedList;
+
+		OpenList.push_back(StartTile);
+
+		while (OpenList.size > 0)
+		{
+			Tile* SmallestF = nullptr;
+			int SmallestFPos = 0;
+
+			//Find the Tile in the open list with the smallest f value
+			for (int i = 0; i < OpenList.size; ++i)
+			{
+				Tile * t = OpenList[i];
+
+				if (!SmallestF || SmallestF->f > t->f)
+				{
+					SmallestF = t;
+					SmallestFPos = i;
+				}
+			}
+			//Remove the Tile with the smallest f value from the open list
+			OpenList.erase(OpenList.begin() + SmallestFPos);
+
+			vector<Tile*> Neighbors;
+			//Add if not blocked
+			Neighbors.push_back(&Map[(SmallestF->y - 1) * MapWidth + (SmallestF->x)]);
+			Neighbors.push_back(&Map[(SmallestF->y + 1) * MapWidth + (SmallestF->x)]);
+			Neighbors.push_back(&Map[(SmallestF->y) * MapWidth + (SmallestF->x - 1)]);
+			Neighbors.push_back(&Map[(SmallestF->y) * MapWidth + (SmallestF->x + 1)]);
+
+			for (Tile* t : Neighbors)
+			{
+				/*i) if successor is the goal, stop search
+          successor.g = q.g + distance between 
+                              successor and q
+          successor.h = distance from goal to 
+          successor (This can be done using many 
+          ways, we will discuss three heuristics- 
+          Manhattan, Diagonal and Euclidean 
+          Heuristics)
+          
+          successor.f = successor.g + successor.h
+
+        ii) if a node with the same position as 
+            successor is in the OPEN list which has a 
+           lower f than successor, skip this successor
+
+        iii) if a node with the same position as 
+            successor  is in the CLOSED list which has
+            a lower f than successor, skip this successor
+            otherwise, add  the node to the open list*/
+			}
+
+			ClosedList.push_back(SmallestF);
+		}
+	}
+};
 
 int main()
 {
