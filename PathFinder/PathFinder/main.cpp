efe268f9f1f7e1d852dfa995a927ce5d1d76b567
Thomas Eberhart <thomas.eberhart@oit.edu>
2020-07-31 10:57:04 -0700

    Cleaned up streak code and added some minor documentation.

--- PathFinder/PathFinder/main.cpp
+++ PathFinder/PathFinder/main.cpp
@@ -6,26 +6,32 @@ using std::vector;
 
 enum TileState
 {
-	EMPTY = 0,
-	START,
-	BLOCKED,
-	END,
-	PATHED
+	EMPTY = 0, //Empty tile that can be used for pathing
+	START, //Start of the path
+	BLOCKED, //Obstacle that cannot be used for pathing
+	END, //End of the path
+	PATHED //Empty tile that has been used for pathing
+};
+
+enum StreakState
+{
+	INACTIVE, //No active streak
+	BLANK, //Current streak is placing empty tiles
+	BLOCK //Current streak is placing blocking tiles
 };
 
 class Tile
 {
 public:
-	Tile(int X, int Y, TileState s)
+	Tile(int _x, int _y, TileState s)
 	{
 		state = s;
-		x = X;
-		y = Y;
+		x = _x;
+		y = _y;
 		g = 1;
 		h = 0;
 		f = INT_MAX;
 		parent = nullptr;
-		ChangedThisStreak = false;
 	}
 	TileState state;
 	int x;
@@ -34,7 +40,6 @@ public:
 	int h;
 	int f;
 	Tile* parent;
-	bool ChangedThisStreak;
 };
 
 class Finder : public olc::PixelGameEngine
@@ -46,18 +51,17 @@ public:
 	}
 
 public:
-	int AppWidth = ScreenWidth();
-	int AppHeight = ScreenHeight();
-	int MapWidth = 64;
-	int MapHeight = 36;
-	int TileSize = 16;
-	Tile * StartTile = nullptr;
-	Tile * EndTile = nullptr;
+	int MapWidth = 64; //Map is 64 tiles wide
+	int MapHeight = 36; //Map is 36 tiles high
+	int TileSize = 16; //Each tile is 16 pixels
+	Tile* StartTile = nullptr;
+	Tile* EndTile = nullptr;
 	vector<Tile> Map;
-	TileState StreakState = PATHED; //When a streak occurs, only allow changing one state to another
+	StreakState StreakState = INACTIVE; //When a streak occurs, only allow changing one state to another
 
 	bool OnUserCreate() override
 	{
+		//Create the map consisting of empty tiles
 		for (int i = 0; i < MapWidth * MapHeight; ++i)
 		{
 			Map.push_back(Tile(i % MapWidth, i / MapWidth, EMPTY));
@@ -67,72 +71,75 @@ public:
 
 	bool OnUserUpdate(float fElapsedTime) override
 	{
+		//Clear the screen to black
 		Clear(olc::BLACK);
-		
+
+		//Clicking when CTRL is held will place a start or end tile
 		if (GetKey(olc::CTRL).bHeld)
 		{
+			//Get the index into the Map vector by converting pixel space coordinates into tile space
 			int index = (GetMouseY() / TileSize) * MapWidth + (GetMouseX() / TileSize);
-			if (GetMouse(0).bPressed)
+
+			if (GetMouse(0).bPressed && Map[index].state != START && Map[index].state != END)
 			{
+				//If the start tile has been placed somewhere else already, make sure to remove it first
 				if (StartTile != nullptr) StartTile->state = EMPTY;
+
+				//Set the state of the new start tile and point the StartTile pointer to this tile
 				Map[index].state = START;
 				StartTile = &Map[index];
 
+				//If both the start and end tiles have been placed then do the algorithm
 				if (StartTile && EndTile)
-				{
 					DoAStar();
-				}
 			}
-			else if (GetMouse(1).bPressed)
+			else if (GetMouse(1).bPressed && Map[index].state != END && Map[index].state != START)
 			{
+				//If the end tile has been placed somewhere else already, make sure to remove it first
 				if (EndTile != nullptr) EndTile->state = EMPTY;
+
+				//Set the state of the new end tile and point the EndTile pointer to this tile
 				Map[index].state = END;
 				EndTile = &Map[index];
 
+				//If both the start and end tiles have been placed then do the algorithm
 				if (StartTile && EndTile)
-				{
 					DoAStar();
-				}
 			}
 		}
 		else if (GetMouse(0).bHeld)
 		{
+			//Get the index into the Map vector by converting pixel space coordinates into tile space
 			int index = (GetMouseY() / TileSize) * MapWidth + (GetMouseX() / TileSize);
 
+			//If this is the first frame that the left mouse button has been pressed then start a streak
 			if (GetMouse(0).bPressed)
 			{
-				StreakState = Map[index].state;
+				if (Map[index].state == EMPTY || Map[index].state == PATHED)
+					StreakState = BLANK;
+				else if (Map[index].state == BLOCKED)
+					StreakState = BLOCK;
 			}
 
-			if (!Map[index].ChangedThisStreak)
-			{
-				if ((Map[index].state == EMPTY || Map[index].state == PATHED) && StreakState != BLOCKED)
-				{
-					Map[index].state = BLOCKED;
-					Map[index].ChangedThisStreak = true;
-				}
-				else if (Map[index].state == BLOCKED && StreakState != EMPTY)
-				{
-					Map[index].state = EMPTY;
-					Map[index].ChangedThisStreak = true;
-				}
+			//Only change the current tile if it matches the current streak
+			if ((Map[index].state == EMPTY || Map[index].state == PATHED) && StreakState != BLOCK)
+				Map[index].state = BLOCKED;
+			else if (Map[index].state == BLOCKED && StreakState != BLANK)
+				Map[index].state = EMPTY;
 
-				if (StartTile && EndTile)
-				{
-					DoAStar();
-				}
-			}
+			//If both the start and end tiles have been placed then do the algorithm
+			if (StartTile && EndTile)
+				DoAStar();
+
 		}
 
+		//End the streak when the mouse is released
 		if (GetMouse(0).bReleased)
 		{
-			for (int i = 0; i < MapWidth * MapHeight; ++i)
-			{
-				Map[i].ChangedThisStreak = false;
-			}
-			StreakState = PATHED;
+			StreakState = INACTIVE;
 		}
 
+		//Drawing the map to the screen
 		for (int i = 0; i < MapWidth; ++i)
 		{
 			for (int j = 0; j < MapHeight; ++j)
@@ -166,9 +173,10 @@ public:
 
 	void DrawPath(vector<Tile*> Path)
 	{
+		//Set the state of each tile in the path that isn't the start or end state to PATHED
 		for (Tile* t : Path)
 		{
-			if (t->state != START && t->state != END) 
+			if (t->state != START && t->state != END)
 				t->state = PATHED;
 		}
 	}
@@ -208,7 +216,7 @@ public:
 			//Find the Tile in the open list with the smallest f value
 			for (int i = 0; i < OpenList.size(); ++i)
 			{
-				Tile * t = OpenList[i];
+				Tile* t = OpenList[i];
 
 				if (!SmallestF || SmallestF->f > t->f)
 				{
@@ -225,16 +233,27 @@ public:
 			Tile* Left = nullptr;
 			Tile* Right = nullptr;
 
-			if (SmallestF->y > 0) Above = &Map[(SmallestF->y - 1) * MapWidth + (SmallestF->x)];
-			if (SmallestF->y < MapHeight - 1) Below = &Map[(SmallestF->y + 1) * MapWidth + (SmallestF->x)];
-			if (SmallestF->x > 0) Left = &Map[(SmallestF->y) * MapWidth + (SmallestF->x - 1)];
-			if (SmallestF->x < MapWidth - 1) Right = &Map[(SmallestF->y) * MapWidth + (SmallestF->x + 1)];
+			if (SmallestF->y > 0) 
+				Above = &Map[(SmallestF->y - 1) * MapWidth + (SmallestF->x)];
+
+			if (SmallestF->y < MapHeight - 1) 
+				Below = &Map[(SmallestF->y + 1) * MapWidth + (SmallestF->x)];
+
+			if (SmallestF->x > 0) 
+				Left = &Map[(SmallestF->y) * MapWidth + (SmallestF->x - 1)];
+
+			if (SmallestF->x < MapWidth - 1) 
+				Right = &Map[(SmallestF->y) * MapWidth + (SmallestF->x + 1)];
 
 
-			if (Above && Above->state != BLOCKED && Above->y < SmallestF->y) Neighbors.push_back(Above);
-			if (Below && Below->state != BLOCKED && Below->y > SmallestF->y) Neighbors.push_back(Below);
-			if (Left && Left->state != BLOCKED && Left->x < SmallestF->x) Neighbors.push_back(Left);
-			if (Right && Right->state != BLOCKED && Right->x > SmallestF->x) Neighbors.push_back(Right);
+			if (Above && Above->state != BLOCKED && Above->y < SmallestF->y) 
+				Neighbors.push_back(Above);
+			if (Below && Below->state != BLOCKED && Below->y > SmallestF->y) 
+				Neighbors.push_back(Below);
+			if (Left && Left->state != BLOCKED && Left->x < SmallestF->x) 
+				Neighbors.push_back(Left);
+			if (Right && Right->state != BLOCKED && Right->x > SmallestF->x) 
+				Neighbors.push_back(Right);
 
 			for (Tile* t : Neighbors)
 			{
@@ -244,7 +263,7 @@ public:
 					t->parent = SmallestF;
 					vector<Tile*> Path;
 					Tile* itr = EndTile;
-					
+
 					while (itr != nullptr)
 					{
 						Path.push_back(itr);
@@ -261,7 +280,7 @@ public:
 					t->h = GetDistance(EndTile, t);
 					int NewF = t->g + t->h;
 
-					
+
 					if (t->f > NewF)
 					{
 						OpenList.push_back(t);
@@ -273,15 +292,15 @@ public:
 
 			ClosedList.push_back(SmallestF);
 		}
-		return vector<Tile*>();
+		return vector<Tile*>(); //Return empty path if it cannot be found
 	}
 };
 
 int main()
 {
-	Finder demo;
-	if (demo.Construct(1024, 576, 1, 1))
-		demo.Start();
+	Finder finder;
+	if (finder.Construct(1024, 576, 1, 1))
+		finder.Start();
 
 	return 0;
 }
